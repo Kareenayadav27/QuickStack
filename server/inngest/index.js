bb7a5ad2aa8c8217ed56bb7c2ddbd5b2ce9ee67b
Kareenayadav27 <yadavkareena47@gmail.com>
2026-07-27 23:55:37 +0530

    Fix Inngest user sync

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -21,7 +21,7 @@ const syncUserCreation = inngest.createFunction(
             name:first_name+' '+last_name,
             image:image_url
         }
-        onsole.log("User data:", userData);
+        console.log("User data:", userData);
 
     const user = await User.create(userData);
 
